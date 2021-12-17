8885c40502dd8d2da3c339b10edeef903fdd227c
Guri0711 <gurcharan0711@gmail.com>
2021-12-17 17:07:01 +1300

    My first coding

---
+++ December2021/December2021/ProgramBase.cs
@@ -0,0 +1,24 @@
+﻿namespace December
+{
+    public class ProgramBase
+    {
+        public static void XYZ(string[] args)
+        {
+            // Each Solution must have only single Main Method, Main method is the entry point
+
+
+            // open chrome browser
+
+            // launch turnup portal
+
+            // identify username textbox and enter valid username
+
+            // identify the password textbox and enter valid password
+
+            // click on login button 
+
+            // check if user  is logged in successfully
+
+        }
+    }
+}
